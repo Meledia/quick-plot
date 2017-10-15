5c3b2af0c6285f41a5274e824ddbfb6c67368f0b
Christian Gaetano <hello@christiangaetano.com>
2017-10-15 10:39:25 -0700

    Add flowtype support

--- src/lib/PlotArea.js
+++ src/lib/PlotArea.js
@@ -1,12 +1,32 @@
+// @flow
+
+import Canvas from 'canvas-prebuilt';
+
+type plotSize = {
+  width: number,
+  height: number,
+};
+
+type plotAreaOptions = {
+  width: ?number,
+  height: ?number,
+};
+
 export default class PlotArea {
-  constructor(options) {
+  size: plotSize;
+  surface: Object;
+
+  constructor(options: plotAreaOptions) {
     // Default to an empty object if no options specified
-    const passedOpts = options || {};
+    const passedOpts: plotAreaOptions = options || {};
 
     // Store some options in this instance for calculations later on
     this.size = {
       width: passedOpts.width || 250,
       height: passedOpts.height || passedOpts.width || 250, // default to a 250 x 250 square
     };
+
+    // Save the drawing surface to class instance
+    this.surface = new Canvas(this.size.width, this.size.height);
   }
 }
